5351debd3a858c2003ec87db806c8a0d7da76e04
Luca Bello <luca.bello@canonical.com>
2025-01-13 11:14:54 +0100

    chore: trigger ci

--- src/charm.py
+++ src/charm.py
@@ -11,7 +11,7 @@ charm contents needs to be unique (i.e. different from all previous
 revisions) in order to create a new revision on Charmhub.
 
 You can increase the following number to guarantee unique contents.
-Number: 42
+Number: 43
 """
 
 import logging

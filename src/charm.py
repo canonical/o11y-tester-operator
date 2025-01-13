5f5e5ace37471b48e33bf28845eaccdfcf52e2a3
Luca Bello <luca.bello@canonical.com>
2025-01-13 10:27:47 +0100

    chore: trigger ci

--- src/charm.py
+++ src/charm.py
@@ -6,7 +6,7 @@
 
 """Charm the service.
 
-Some new unique string. :)
+Some new unique string. :D
 
 Refer to the following tutorial that will help you
 develop a new k8s charm using the Operator Framework:

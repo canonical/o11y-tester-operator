6c510830bca9254b679b380bde1493080487938c
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-07-21 12:11:09 +0200

    touch

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -53,6 +53,7 @@ async def charm(ops_test: OpsTest) -> str:
 
 @pytest.fixture(scope="session", autouse=True)
 def dump_logs():
+    # foo
     yield
     logs = Path(REPO_ROOT)/".logs"
     logs.mkdir(exist_ok=True)

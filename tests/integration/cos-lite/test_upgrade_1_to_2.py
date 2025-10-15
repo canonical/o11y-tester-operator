0ef48b89d41cab0fb93859d314574c5a7a3fdbaa
Michael Thamm <mike.thamm@canonical.com>
2025-10-15 16:59:05 -0400

    chore

--- tests/integration/cos-lite/test_upgrade_1_to_2.py
+++ tests/integration/cos-lite/test_upgrade_1_to_2.py
@@ -8,7 +8,7 @@ COS_TF_FILE = "cos-lite.tf"
 @pytest.mark.setup
 def test_deploy(tf_manager, cos_model: jubilant.Juju):
     tf_manager.init(COS_TF_FILE)
-    tf_manager.apply(COS_TF_FILE, cos_model, channel="1/stable", internal_tls=True)
+    tf_manager.apply(COS_TF_FILE, cos_model, channel="1/stable")
 
 
 @pytest.mark.abort_on_fail
@@ -18,7 +18,8 @@ def test_upgrade(tf_manager, cos_model: jubilant.Juju):
 
 @pytest.mark.abort_on_fail
 @pytest.mark.skip(
-    reason='Traefik hits error state on destroying the model due to hook failed: "receive-ca-cert-relation-broken"'
+    reason='Traefik hits error state on destroying the model due to hook failed: "'
+    'receive-ca-cert-relation-broken"'
 )
 def test_destroy(cos_model: jubilant.Juju):
     pass
